2fcb97fef902cd0580b68d9f91c30b9943c712be
Kaalypsia <leroylau12@gmail.com>
2025-04-02 14:09:42 +0200

    Ou users

--- IMPORT_Users.ps1
+++ IMPORT_Users.ps1
@@ -9,7 +9,7 @@ $index = 1
 foreach ($user in $users) {
     $username = $user.Username
     $password = ConvertTo-SecureString $user.password -AsPlainText -Force
-    $ou = if ($index -le 450) { "users" } elseif ($index -le 470) { "IT" } else { "extern" }
+ #   $ou = if ($index -le 450) { "users" } elseif ($index -le 470) { "IT" } else { "extern" }
     $displayName = "$($user.nom) $($user.prenom)"
 
     # Créer l'utilisateur
@@ -19,7 +19,7 @@ foreach ($user in $users) {
         -Surname $user.nom `
         -PasswordNeverExpires $true `
         -AccountPassword $password `
-        -Enabled $true -Path "OU=$ou,OU=Buissounet,DC=MonBuisson,DC=com"
+        -Enabled $true -Path "OU=users,OU=Buissounet,DC=MonBuisson,DC=com"
 
     # Incrémenter l'index
     $index++
