b72c527a762cfcad007be215e28b59b4fff51668
Kaalypsia <leroylau12@gmail.com>
2025-04-02 12:21:17 +0200

    Maj lien tableur

--- IMPORT_Users.ps1
+++ IMPORT_Users.ps1
@@ -1,18 +1,24 @@
 # Script pour l'importation des utilisateurs à partir d'un fichier CSV
 
 # Charger les utilisateurs depuis le fichier CSV
-$csvFile = "C:\chemin\vers\users.csv"  # À personnaliser
+$csvFile = "C:\scripts\users.csv"  # À personnaliser
 $users = Import-Csv -Path $csvFile
 
 # Créer les utilisateurs et les ajouter aux OUs appropriées
 $index = 1
 foreach ($user in $users) {
     $username = $user.Username
-    $password = ConvertTo-SecureString $user.Password -AsPlainText -Force
+    $password = ConvertTo-SecureString $user.password -AsPlainText -Force
     $ou = if ($index -le 450) { "users" } elseif ($index -le 470) { "IT" } else { "extern" }
 
     # Créer l'utilisateur
-    New-ADUser -SamAccountName $username -UserPrincipalName "$username@MonBuisson.com" -Name "$($user.Nom) $($user.Prénom)" -GivenName $user.Prénom -Surname $user.Nom -PasswordNeverExpires $true -AccountPassword $password -Enabled $true -Path "OU=$ou,DC=MonBuisson,DC=com"
+    New-ADUser -SamAccountName $username -UserPrincipalName "$username@MonBuisson.com"
+        -Name "$($user.nom) $($user.prenom)"
+        -GivenName $user.prenom
+        -Surname $user.nom
+        -PasswordNeverExpires $true
+        -AccountPassword $password
+        -Enabled $true -Path "OU=$ou,DC=MonBuisson,DC=com"
 
     # Incrémenter l'index
     $index++
